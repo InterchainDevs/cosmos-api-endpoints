80cb64ea84fa4317735c2d313641141f0d26ebf3
Raul Bernal <bernal.raul@gmail.com>
2024-07-26 10:18:41 +0200

    clean config file

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ VESTING_FILE = 'vesting.json'
 CHAIN_FILE = 'chain_metrics.json'
 HTML_DOC = 'index.html'
 HOST= '0.0.0.0'
-PORT= 40420
+PORT= 3030
 TIME_TO_SLEEP = 60 * 60 # 1 hour
 
 # Chain config
@@ -25,13 +25,3 @@ SUPPLY_TOTAL_ALL = LCD + '/cosmos/bank/v1beta1/supply?pagination.limit=3000&pagi
 SUPPLY_CIRCULATING = '' # Total_Denom - Bonded - VESTED_ACCOUNTS_BALANCE
 SUPPLY_RICHLIST = LCD + '/cosmos/bank/v1beta1/balances/' # addr/bcna1khyrv2kcx5qsqx46pt78dk3wp8naqnygke0zcl + '/by_denom?denom=' + DENOM
 SUPPLY_ACCOUNTS = LCD + '/cosmos/auth/v1beta1/accounts?pagination.limit=5000&pagination.count_total=true'
-# APR = ''
-
-
-
-# https://api2.bitcanna.io/api/rest/supply/apr
-# https://api2.bitcanna.io/api/rest/supply/circulating
-# https://api2.bitcanna.io/api/rest/supply/bonded
-# https://api2.bitcanna.io/api/rest/supply/total
-# https://api2.bitcanna.io/api/rest/supply/total_all
-# https://api2.bitcanna.io/api/rest/supply/richlist
